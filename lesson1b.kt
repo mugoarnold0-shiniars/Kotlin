a808543b8b7b4cc8c33eae0391d753bf3f71a40a
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-29 11:56:46 +0300

    lesson 1b

---
+++ lesson1b.kt
@@ -0,0 +1,15 @@
+fun main(){
+    //  we shall specify the data types of the variables 
+    val number1 : Int = 5
+    val number2 : Double=90.23
+    val letter : Char = 'B'
+    val paidLoan : Boolean = false
+    val name : String = "Arnold"
+
+    println(number1)
+    println(number2)
+    println(letter)
+    println(paidLoan)
+    println(name)
+
+}
