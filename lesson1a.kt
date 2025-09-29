b3742aae2e72470b2d9191d6f7ece00ece977fb1
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-29 11:37:25 +0300

    Kotlin

---
+++ lesson1a.kt
@@ -0,0 +1,20 @@
+fun main(){
+    // This is a comment
+    // Variable is a container that holds data values
+    //  in kotlin we have two main type of initializers i.e:var and val
+    // Var is a initializer of values that keeps on changing
+    // Val is a initializer of values that don't change
+
+    val name = "Arnold" // string~ it is a series of characters
+    val number1 = 5 //Intergerdata type
+    val number2 = 7.85  //Double`~ a number with a decimal
+    val letter ="K"      //Char ~character(it is a single letter of the aphabet)
+    val isRegistered=true //Boolean
+
+
+    println(name)
+    println(number1)
+    println(number2)
+    println(letter)
+    println(isRegistered)
+}
