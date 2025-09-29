81961f274cbc7540b20080c08b31d588c16eaa43
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-29 12:17:11 +0300

    Assignment1a

---
+++ Assignment.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val number1 : Int=50
+    val number2 : Double=4.5
+
+    val total : Double=number1*number2
+    val divide :Double = number1/number2
+
+    println("The product is the numbers is: $total")
+    println("The division  is the numbers is: $divide")
+
+}
+
+
+
